c3561d0ff6f500bad78757258b10af643ea5ef5b
daycat <iyasmalan@gmail.com>
2022-01-28 19:34:35 +0800

    Create 2021-jan-bronze-1.cpp

---
+++ 2021 January/2021-jan-bronze-1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string sequence, heard;
+    /***
+    for(int i=0; i<26; i++){
+        char temp;
+        cin >> temp;
+        charsequence[i] = (int) temp - 97;
+        }
+        ***/
+
+    cin >> sequence >> heard;
+    string allchars = "";
+    for (int runs =1 ; true; runs ++){
+        int foundnum = 0;
+        allchars += sequence;
+        for (int i=0; i< (int) allchars.size(); i++){
+            if(allchars[i] == heard[foundnum]){
+            foundnum++;
+            }
+        }
+        if (foundnum == (int) heard.size()){
+        cout << runs;
+        return 0;
+        }
+
+    } 
+
+
+}
